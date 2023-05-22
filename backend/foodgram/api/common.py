46081487305314f3f51a0fe6e477862700b1fe94
Guzelik-Victor <vguzelik@yandex.ru>
2023-05-22 10:10:56 +0300

    bugfix

--- backend/foodgram/api/common.py
+++ backend/foodgram/api/common.py
@@ -51,4 +51,5 @@ def create_update_instance_recipe(recipe, ingredients, tags):
             TagRecipe(recipe=recipe, tag=tag)
         )
     TagRecipe.objects.bulk_create(obj_tag_recipe)
+
     return
